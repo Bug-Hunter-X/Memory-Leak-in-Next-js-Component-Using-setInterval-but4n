c87df4b2e20a29c3044310ec2b29f05cb1546400
BugHunter-X <askzenai@gmail.com>
2025-01-03 07:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,29 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <MyComponent/>
+    </div>
+  );
+}
+
+// components/MyComponent.js
+export default function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This is the problematic section.
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>Count: {count}</div>
+  );
+}
+```
